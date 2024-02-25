cd66364347265508065b9be9f7d3c911dc94080c
Artem <artyom9302@gmail.com>
2024-02-25 18:38:16 +0200

    feat: draft header mobile menu

---
+++ components/the-header/TheHeader.vue
@@ -0,0 +1,194 @@
+<script lang="ts" setup>
+const localePath = useLocalePath();
+</script>
+
+<template>
+  <header class="header">
+    <div class="header__container">
+      <button
+        class="header__menu-button"
+        type="button"
+      >
+        <nuxt-icon name="menu" />
+      </button>
+
+      <NuxtLink
+        class="header__logo"
+        :to="localePath('index')"
+      >
+        <img
+          class="header__logo-image"
+          src="/logo.svg"
+          height="24rem"
+          width="auto"
+          :alt="$t('logotype')"
+        >
+      </NuxtLink>
+
+      <nav class="header__navigation">
+        <NuxtLink
+          class="header__link"
+          :to="localePath('index')"
+        >
+          {{ $t('home') }}
+        </NuxtLink>
+
+        <NuxtLink
+          class="header__link"
+          no-prefetch
+          :to="localePath('movies')"
+        >
+          {{ $t('movies') }}
+        </NuxtLink>
+
+        <NuxtLink
+          class="header__link"
+          no-prefetch
+          :to="localePath('series')"
+        >
+          {{ $t('series') }}
+        </NuxtLink>
+
+        <NuxtLink
+          class="header__link"
+          no-prefetch
+          :to="localePath('persons')"
+        >
+          {{ $t('persons') }}
+        </NuxtLink>
+      </nav>
+
+      <div class="header__actions">
+        <button
+          class="header__search-button"
+          type="button"
+        >
+          <nuxt-icon name="search" />
+        </button>
+
+        <button
+          class="header__login-button"
+          type="button"
+        >
+          <nuxt-icon name="login" />
+        </button>
+
+        <button
+          class="header__locales-button"
+          type="button"
+        >
+          En
+        </button>
+      </div>
+    </div>
+
+    <TheHeaderMobileMenu />
+  </header>
+</template>
+
+<style lang="sass" scoped>
+.header
+  position: relative
+
+  &__container
+    display: flex
+    align-items: center
+    height: 6rem
+    background-color: $primary-900
+    -webkit-box-shadow: 0px 2px 10px 0px hex-with-alpha($black, 0.25)
+    -moz-box-shadow: 0px 2px 10px 0px hex-with-alpha($black, 0.25)
+    box-shadow: 0px 2px 10px 0px hex-with-alpha($black, 0.25)
+    column-gap: 6rem
+    padding: 0 3.2rem
+    position: relative
+    z-index: 2
+
+    @include media-breakpoint-down(sm)
+      column-gap: 2rem
+      padding: 0 2.4rem
+
+  &__logo
+    height: 2.4rem
+    width: 17.2rem
+
+    @include media-breakpoint-down(sm)
+      flex: 1
+      height: 2rem
+
+    &-image
+      width: 100%
+      height: 100%
+
+  &__navigation
+    flex: 1
+    display: none
+    display: flex
+    column-gap: 2.4rem
+
+    @include media-breakpoint-down(sm)
+      display: none
+
+  &__link
+    font-size: 1.8rem
+    font-weight: 600
+    color: $primary-300
+    transition: color 0.25s
+    cursor: pointer
+
+    &:hover,
+    &.router-link-active
+      color: $primary-50
+
+  &__actions
+    display: flex
+    align-items: center
+    column-gap: 2.4rem
+
+  &__login-button,
+  &__search-button,
+  &__menu-button
+    background-color: transparent
+
+    & .nuxt-icon
+      width: 2.4rem
+      height: 2.4rem
+      color: $primary-300
+      transition: color 0.25s
+
+    @include hover-adaptive()
+      & .nuxt-icon
+        color: $primary-50
+
+  &__menu-button
+    display: none
+
+    @include media-breakpoint-down(sm)
+      display: initial
+
+  &__login-button
+    @include media-breakpoint-down(sm)
+      display: none
+
+  &__locales-button
+    display: flex
+    justify-content: center
+    align-items: center
+    width: 3.2rem
+    height: 3.2rem
+    background-color: transparent
+    font-size: 1.4rem
+    font-weight: 600
+    text-transform: uppercase
+    color: $primary-300
+    border: 2px solid $primary-300
+    border-radius: 0.8rem
+    transition: background-color 0.25s, color 0.25s, border-color 0.25s
+
+    @include hover-adaptive()
+      background-color: $primary-50
+      color: $primary-900
+      border-color: $primary-50
+
+    @include media-breakpoint-down(sm)
+      display: none
+</style>
---
+++ components/the-header/TheHeaderMobileMenu.vue
@@ -0,0 +1,163 @@
+<script lang="ts" setup>
+const localePath = useLocalePath();
+</script>
+
+<template>
+  <div class="mobile-menu">
+    <nav class="mobile-menu__navigation">
+      <NuxtLink
+        class="mobile-menu__link"
+        :to="localePath('index')"
+      >
+        {{ $t('home') }}
+      </NuxtLink>
+
+      <NuxtLink
+        class="mobile-menu__link"
+        no-prefetch
+        :to="localePath('movies')"
+      >
+        {{ $t('movies') }}
+      </NuxtLink>
+
+      <NuxtLink
+        class="mobile-menu__link"
+        no-prefetch
+        :to="localePath('series')"
+      >
+        {{ $t('series') }}
+      </NuxtLink>
+
+      <NuxtLink
+        class="mobile-menu__link"
+        no-prefetch
+        :to="localePath('persons')"
+      >
+        {{ $t('persons') }}
+      </NuxtLink>
+    </nav>
+
+    <div class="mobile-menu__actions">
+      <div class="mobile-menu__login">
+        <button
+          class="mobile-menu__login-button"
+          type="button"
+        >
+          <nuxt-icon name="login" />
+          <span>{{ $t('Sign in') }}</span>
+        </button>
+      </div>
+
+      <div class="mobile-menu__locales">
+        <button
+          class="mobile-menu__locales-button mobile-menu__locales-button--active"
+          type="button"
+        >
+          En
+        </button>
+
+        <button
+          class="mobile-menu__locales-button"
+          type="button"
+        >
+          UK
+        </button>
+
+        <button
+          class="mobile-menu__locales-button"
+          type="button"
+        >
+          RU
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="sass" scoped>
+.mobile-menu
+  display: flex
+  flex-direction: column
+  width: 100%
+  min-height: calc(100vh - 6rem)
+  background-color: $primary-900
+  position: absolute
+  z-index: 1
+  top: 6rem
+  left: 0
+
+  &__navigation
+    display: flex
+    flex-direction: column
+    flex: 1
+    row-gap: 1.6rem
+    border-bottom: solid 1px $primary-300
+    padding: 2.4rem
+
+  &__link
+    font-size: 2.4rem
+    font-weight: 600
+    color: $primary-300
+    transition: color 0.25s
+    cursor: pointer
+
+    &:hover,
+    &.router-link-active
+      color: $primary-50
+
+  &__actions
+    padding: 2.4rem 2.4rem 6.8rem
+
+  &__login
+    display: flex
+    align-items: center
+    margin-bottom: 2.4rem
+
+  &__login-button
+    background-color: transparent
+    font-size:  2rem
+    color: $primary-300
+    transition: color 0.25s
+
+    & .nuxt-icon
+      width: 3.2rem
+      height: 3.2rem
+      color: $primary-300
+      margin-right: 0.8rem
+      transition: color 0.25s
+
+    @include hover-adaptive()
+      color: $primary-50
+
+      & .nuxt-icon
+        color: $primary-50
+
+  &__locales
+    display: flex
+    column-gap: 1.6rem
+
+  &__locales-button
+    display: flex
+    justify-content: center
+    align-items: center
+    width: 3.6rem
+    height: 3.6rem
+    background-color: transparent
+    font-size: 1.4rem
+    font-weight: 600
+    text-transform: uppercase
+    color: $primary-300
+    border: 2px solid $primary-300
+    border-radius: 0.8rem
+    transition: background-color 0.25s, color 0.25s, border-color 0.25s
+
+    @include hover-adaptive()
+      background-color: $primary-50
+      color: $primary-900
+      border-color: $primary-50
+
+    &--active
+      background-color: $primary-50
+      color: $primary-900
+      border-color: $primary-50
+</style>
